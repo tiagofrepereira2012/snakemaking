e1f08b0313f2eba15f4eef2db3a28d3f321fcb5b
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2019-11-26 17:33:03 +0100

    Testing SGE

--- parsl/workflows/parallel.py
+++ parsl/workflows/parallel.py
@@ -1,28 +1,42 @@
 ########## COMPUTING PI EXAMPLE ########
 
 
+# We have to load some config
+##print(config)
+#parsl.load(config)
+
+
 import parsl
 import os
 from parsl.app.app import python_app, bash_app
 from parsl.configs.local_threads import config
 from parsl.config import Config
+from parsl.providers import LocalProvider, GridEngineProvider
 from parsl.executors.threads import ThreadPoolExecutor
+from parsl.executors import HighThroughputExecutor
+from parsl.channels import LocalChannel, SSHChannel
+from parsl.addresses import address_by_query
+
 import numpy
 
-# We have to load some config
-##print(config)
-#parsl.load(config)
+THREADS = 8
 
-THREADS = 1
+#config = Config(executors=[ThreadPoolExecutor(max_threads=THREADS, label='local_threads')])
 
-config= Config(executors=[ThreadPoolExecutor(max_threads=THREADS,
-                                 label='local_threads')])
+
+config= Config(executors=[HighThroughputExecutor(
+                              label='local_threads',
+                              address=address_by_query(),
+                              provider=GridEngineProvider(channel=LocalChannel(), worker_init="source /idiap/user/tpereira/conda/bin/activate /idiap/user/tpereira/conda/envs/snakemaking")
+                              )])
+
 parsl.load(config)
 
 
 # App that estimates pi by placing points in a box
 @python_app
 def pi(num_points):
+    print("############## PI #############")
     from random import random
     #import ipdb; ipdb.set_trace()
     inside = 0
@@ -47,5 +61,5 @@ mean_pi  = mean(*numbers)
 
 # Print the results
 #print("a: {:.5f} b: {:.5f} c: {:.5f}".format(a.result(), b.result(), c.result()))
-import ipdb; ipdb.set_trace()
+#import ipdb; ipdb.set_trace()
 print("Average: {:.5f}".format(mean_pi.result()))
