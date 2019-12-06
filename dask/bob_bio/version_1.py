77326dd30c4afd3d15e0d0a1f295e91f5dcb4081
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2019-12-06 14:54:51 +0100

    Working on a generic database interface

--- dask/bob_bio/version_1.py
+++ dask/bob_bio/version_1.py
@@ -12,6 +12,8 @@ from time import sleep
 from utils import read_biofiles, amend_path, split_data 
 import itertools
 
+from samples import BiometricSamples
+
 
 NODES = 1
 
@@ -23,20 +25,82 @@ client = Client(cluster)  # start local workers as threads
 
 ### SGE
 
-#from dask_jobqueue import PBSCluster
-#cluster = PBSCluster(cores=2, memory="4GB", queue="all.q")
-#cluster.scale(NODES)
-#client = Client(cluster)
+"""
+from dask_jobqueue import PBSCluster, SGECluster
 
+q1d_resource_spec = "q_1day=TRUE,io_big=TRUE"
+gpu_resource_spec = "q_gpu=TRUE"
+
+
+cluster = SGECluster(queue="q_1day", memory='4GB', cores=2,
+        log_directory="/idiap/user/tpereira/github/snakemaking/dask/bob_bio/logs",
+        local_directory="/idiap/user/tpereira/github/snakemaking/dask/bob_bio/logs",
+        resource_spec=q1d_resource_spec
+        )
+"""     
+
+#############
+# HERE I'M TESTING HETEROGENEOUS JOBS
+#
+
+### NODES q1d
+#for i in range(NODES-1):
+    # GETTING THE SPEC TEMPLATE#
+    #spec = cluster.new_spec
+
+    # PATCHING
+    #spec['options']['resource_spec'] = q1d_resource_spec
+    #cluster.worker_spec[i] = spec
+
+#import ipdb; ipdb.set_trace()    
+#cluster.scale_up(NODES-1)
+
+### NODE GPU
+
+# GETTING THE SPEC TEMPLATE#
+#spec = cluster.new_spec
+
+# PATCHING
+#spec['options']['resource_spec'] = gpu_resource_spec
+#spec['options']['queue'] = 'q_gpu'
+#cluster.worker_spec[i] = spec
+#cluster.scale_up(1)
+
+##############
+#import ipdb; ipdb.set_trace()
+
+cluster.scale_up(10)
+client = Client(cluster)
+
+
+##########
+# HANDLING DATABASE
+##########
 
 experiment_path = "/idiap/user/tpereira/github/snakemaking/dask/bob_bio/test/"
 database_path = "/idiap/group/biometric/databases/biometrics/face2D/orl/"
 database_extension = ".pgm"
 
+
 import bob.bio.face
 db = bob.bio.face.database.AtntBioDatabase(original_directory=database_path,
                                            original_extension=database_extension)
 
+
+# TODO: REPLACE 
+training_samples = BiometricSamples.create_training_samples(db)
+
+template_samples = BiometricSamples.create_biometric_template(db)
+
+#    def create_biometric_template(cls, database, group="dev", protocol="Default"):
+
+#training_objects = amend_path(db.training_files(), database_path, database_extension)
+#enroll_objects = amend_path(db.enroll_files(), database_path, database_extension)
+#probe_objects = amend_path(db.probe_files(), database_path, database_extension)
+
+#training_objects = db.training_files()
+#enroll_objects = db.enroll_files()
+#probe_objects = db.probe_files()
 
 
 def read_bobbiodata(file_name):
@@ -46,46 +110,74 @@ def write_bobbiodata(data, file_name):
     bob.io.base.save(data, file_name)
 
 
-def process_bobbiofile(bio_files=[],
-                       processor=None,
-                       output_path=None,
-                       output_extension=".hdf5",
-                       raw_data=True):
+def cache_bobbio_samples(output_path, output_extension):
+    def decorator(func):
+                       
+        def wrapper(*args, **kwargs):
+
+            biometric_samples = func(*args, **kwargs)
+
+            for b in biometric_samples:
+
+                for o in b.samples:
+                    # Setting the current location of the biofile
+                    o.current_directory = output_path
+                    o.current_extension = output_extension
+
+                    # Saving
+                    file_name = o.make_path(o.current_directory, o.current_extension)
+                    
+                    # If it is already cached, don't do anything
+                    if os.path.exists(file_name):
+                        continue
+
+                    # Save
+                    bob.io.base.create_directories_safe(os.path.dirname(file_name))
+                    write_bobbiodata(o.sample, file_name)
+                    o.sample = None
+
+
+            #import logging
+            #logging.basicConfig()
+            #logger = logging.getLogger()
+
+            #logger.setLevel(logging.INFO)
+            #logger.info("##############")
+            #logger.info("Done")
+            #logger.info("##############")
+
+            return biometric_samples
+             
+        return wrapper
+    return decorator
+
+
+
+def process_bobbiofile(biometric_samples=[],
+                       processor=None):
     """
     Given a list of BioFiles:
       1. Load
       2. Process
       3. Save
     """
-    for f in bio_files:
 
-        # Defining Input/Output
-        input_file  = f.make_path(f.current_directory, f.current_extension)
-        output_file = f.make_path(output_path, output_extension)
+    for b in biometric_samples:
 
-        # TODO: Make a proper check for that
-        if os.path.exists(output_file):
-            f.current_directory = output_path
-            f.current_extension = output_extension     
-            continue
+        for f in b.samples:
 
-        if raw_data:
-            data = f.load(f.current_directory, f.current_extension)
-        else:
-            data = read_bobbiodata(input_file)
-        
-        f.current_directory = output_path
-        f.current_extension = output_extension
- 
-        # Preprocessing
-        processed = processor(data)
+            if f.sample is None:
+                input_file = f.make_path(f.current_directory, f.current_extension)
+                f.sample   = read_bobbiodata(input_file) 
+               
+            # Preprocessing
+            data = f.sample
+            processed = processor(data)
 
-        # Writing       
-        bob.io.base.create_directories_safe(os.path.dirname(output_file))
-        write_bobbiodata(processed, output_file)
+            # Injecting the sample in bio_file
+            f.sample = processed
 
-
-    return bio_files
+    return biometric_samples
 
 
 def train_bobalgorithm(bio_files, algorithm, output_model):
@@ -100,103 +192,92 @@ def train_bobalgorithm(bio_files, algorithm, output_model):
     merged_training_futures = functools.reduce(operator.concat, bio_files)
 
     training_data = read_biofiles(merged_training_futures, bob.io.base.load)
+    
     # TODO: Issue 106
     algorithm.train_projector(training_data, output_model)
   
     return output_model
 
 
-def project_bobalgorithm(bio_files, algorithm, background_model, output_path, output_extension=".hdf5"):
+def project_bobalgorithm(bio_files, algorithm, background_model):
 
     # Loading backgroundmodel
     algorithm.load_projector(background_model)
 
     for f in bio_files:
-        # Defining Input/Output    
-        input_file  = f.make_path(f.current_directory, f.current_extension)
-        output_file = f.make_path(output_path, output_extension)
 
-        # TODO: Make a proper check for that
-        if os.path.exists(output_file):
-            f.current_directory = output_path
-            f.current_extension = output_extension     
-            continue
+        if f.sample is None:
+            input_file = f.make_path(f.current_directory, f.current_extension)
+            f.sample   = read_bobbiodata(input_file) 
 
-        data = f.load(f.current_directory, f.current_extension)
-       
-        f.current_directory = output_path
-        f.current_extension = output_extension
-
+        data = f.sample
         projected = algorithm.project(data)
+        f.sample = data
 
-        bob.io.base.create_directories_safe(os.path.dirname(output_file))
-        write_bobbiodata(projected, output_file)
-
- 
     return bio_files
 
 
 
-#####
-##### TODO: HACKING Working with the idea that objects should know their location
-##### 
 
 
-training_objects = amend_path(db.training_files(), database_path, database_extension)
-enroll_objects = amend_path(db.enroll_files(), database_path, database_extension)
-probe_objects = amend_path(db.probe_files(), database_path, database_extension)
-
-
-
-
+######################################
 ####### BUILDING THE PIPELINE ########
+######################################
 
 
 # 1. PREPROCESS
 preproc_futures = []
 preprocessor = bob.bio.face.preprocessor.Base(color_channel="gray",
                                               dtype = numpy.float64)
-output_path = os.path.join(experiment_path, "./preprocessed")
 
 
 # Initial split
-training_objects_split = split_data(training_objects, NODES)
-enroll_objects_split = split_data(enroll_objects, NODES)
-probe_objects_split = split_data(probe_objects, NODES)
+training_objects_split = split_data(training_samples, NODES)
+template_objects_split = split_data(template_samples, NODES)
+#probe_objects_split = split_data(probe_objects, NODES)
 
 preproc_training_futures = []
 preproc_enroll_futures = []
 preproc_probe_futures = []
-for t_o, e_o, p_o in zip(training_objects_split, enroll_objects_split, probe_objects_split):
+
+
+# DECORATING INPUT AND OUTPUT
+output_path = os.path.join(experiment_path, "./preprocessed")
+decorated_preprocess = cache_bobbio_samples(output_path, ".hdf5")(process_bobbiofile)
+#decorated_preprocess = process_bobbiofile
+
+for t_o, e_o, p_o in zip(training_objects_split, template_objects_split, template_objects_split):
     preproc_training_futures.append(
-            client.submit(process_bobbiofile,
-                          t_o,
-                          preprocessor,
-                          output_path,
-                          raw_data=True
+            client.submit(decorated_preprocess,
+                          biometric_samples=t_o,
+                          processor=preprocessor
                          )
             )
+
 
     preproc_enroll_futures.append(
-            client.submit(process_bobbiofile,
-                          e_o,
-                          preprocessor,
-                          output_path,
-                          raw_data=True
+            client.submit(decorated_preprocess,
+                          biometric_samples=e_o,
+                          processor=preprocessor
                          )
             )
+
     
-    preproc_probe_futures.append(
-            client.submit(process_bobbiofile,
-                          p_o,
-                          preprocessor,
-                          output_path,
-                          raw_data=True
-                         )
-            )
+  
+    
+    #preproc_probe_futures.append(
+    #        client.submit(decorated_preprocess,
+    #                      bio_files=p_o,
+    #                      processor=preprocessor
+    #                     )
+    #        )
 
+for t_o, e_o in zip(preproc_training_futures, preproc_enroll_futures):
+    t_o.result()
+    e_o.result()
 
-
+"""
+ 
 # 2. EXTRACT
 extract_training_futures = []
 extract_enroll_futures = []
@@ -205,111 +286,91 @@ extract_probe_futures = []
 
 extractor = bob.bio.base.extractor.Linearize()
 output_path = os.path.join(experiment_path, "./extracted")
+
+decorated_extractor = cache_bobbio_samples(output_path, ".hdf5")(process_bobbiofile)
+#decorated_extractor = process_bobbiofile
+
+
 for t_o, e_o, p_o in zip(preproc_training_futures, preproc_enroll_futures, preproc_probe_futures):
        
 
     extract_training_futures.append(
-            client.submit(process_bobbiofile,
-                          t_o,
-                          extractor,
-                          output_path,
-                          raw_data=False
+            client.submit(decorated_extractor,
+                          bio_files=t_o,
+                          processor=extractor
                           )
             )
 
     extract_enroll_futures.append(
-            client.submit(process_bobbiofile,
-                          e_o,
-                          extractor,
-                          output_path,
-                          raw_data=False
+            client.submit(decorated_extractor,
+                          bio_files=e_o,
+                          processor=extractor
                           )
             )
 
     extract_probe_futures.append(
-            client.submit(process_bobbiofile,
-                          p_o,
-                          extractor,
-                          output_path,
-                          raw_data=False
+            client.submit(decorated_extractor,
+                          bio_files=p_o,
+                          processor=extractor
                           )
             )
 
 
+#for i, j, k in zip(extract_training_futures, extract_enroll_futures, extract_probe_futures):
+#    i.result()
+#    j.result()
+#    k.result()
+
 
 #3. BACKGROUND MODEL TRAINING
+
 from bob.bio.base.algorithm import PCA
 algorithm = PCA(0.99)
 output_model = os.path.join(experiment_path, "Project.hdf5")
 background_model = client.submit(train_bobalgorithm, extract_training_futures, algorithm, output_model)
 
 
-#4. PROJECTING
 
+#4. PROJECTING
 projected_enroll_futures = []
 projected_probe_futures = []
 
 
+# DECORATING INPUT AND OUTPUT
 output_path = os.path.join(experiment_path, "./projected")
+decorated_projected = cache_bobbio_samples(output_path, ".hdf5")(project_bobalgorithm)
 
 for  e_o, p_o in zip(preproc_enroll_futures, preproc_probe_futures):
 
     projected_enroll_futures.append(
-            client.submit(project_bobalgorithm,
+            client.submit(decorated_projected,
                           e_o,
                           algorithm,
-                          background_model,
-                          output_path)
+                          background_model
+                          )
             )
 
     projected_probe_futures.append(
-            client.submit(project_bobalgorithm,
+            client.submit(decorated_projected,
                           p_o,
                           algorithm,
-                          background_model,
-                          output_path)
+                          background_model
+                         )
             )
+
+for  e_o, p_o in zip(projected_enroll_futures, projected_probe_futures):
+    e_o.result()
+    p_o.result()
+"""
+
 
 
 
 #5. CREATE TEMPLATE
-
+"""
 template_enroll_futures = []
-
 output_path = os.path.join(experiment_path, "./models")
 
 
-"""
-for e_o, p_o in zip(preproc_enroll_futures, preproc_probe_futures):
-
-    projected_enroll_futures.append(
-            client.submit(project_bobalgorithm,
-                          e_o,
-                          algorithm,
-                          background_model,
-                          output_path)
-            )
-
-    projected_probe_futures.append(
-            client.submit(project_bobalgorithm,
-                          p_o,
-                          algorithm,
-                          background_model,
-                          output_path)
-            )
-"""
-
-#for  e_o, p_o in zip(projected_enroll_futures, projected_probe_futures):
-#    e_o.result()
-#    p_o.result()
-
-
-#project_bobalgorithm(bio_files, algorithm, background_model, output_path, output_extension=".hdf5"):
-
-#serialized_machine = pca_model.result()
-#machine = from_dict(serialized_machine)
-
-#print(machine.weights)
-
 pass
-
+"""
